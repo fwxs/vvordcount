8c3bebc3ba66980fb3cb29b63eca8e84dee45f8e
fwxks <mrpcmn@protonmail.com>
2019-07-13 12:51:08 -0300

    New structure

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED= True
+    SECERT_KEY = os.urandom(32)
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
