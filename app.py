48d7957df7611a976c9791f183b4a56e9791d97a
fwxks <mrpcmn@protonmail.com>
2019-07-27 20:37:25 -0300

    Updated UI and error handling.

--- app.py
+++ app.py
@@ -22,6 +22,25 @@ db = SQLAlchemy(app)
 
 queue = Queue(connection=conn)
 
+
+class InvalidURL(Exception):
+    status_code = 400
+
+    def __init__(self, messages, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = "\n".join(messages)
+
+        if status_code is not None:
+            self.status_code = status_code
+
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv["message"] = self.message
+        return rv
+
+
 from models import Result
 
 
@@ -33,7 +52,7 @@ def count_and_save_words(url):
 
     except:
         errors.append("Unable to get URL: Please make sure it's valid and try again")
-        return {"error": errors}
+        return {"error": errors }
 
     # Text processing
     raw = BeautifulSoup(req.text, "html.parser").get_text()
@@ -78,6 +97,12 @@ def count_and_save_words(url):
         return {"error": errors}
 
 
+@app.errorhandler(InvalidURL)
+def handler_invalid_url(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template("index.html")
@@ -97,7 +122,7 @@ def get_counts():
         url = "https://" + url
 
     job = queue.enqueue_call(
-            func=count_and_save_words, args=(url,), result_ttl=5000
+            func=count_and_save_words, args=(url,), result_ttl=500
             )
 
     return job.get_id()
@@ -108,12 +133,16 @@ def get_result(job_key):
     job = Job.fetch(job_key, connection=conn)
 
     if job.is_finished:
+        if not isinstance(job.result, int):
+            raise InvalidURL(job.result["error"], status_code=404)
+
         result = Result.query.filter_by(id=job.result).first()
         results = sorted(
-                    result.result_no_stop_words.items(),
-                    key=operator.itemgetter(1),
-                    reverse=True
-                  )[:10]
+                        result.result_no_stop_words.items(),
+                        key=operator.itemgetter(1),
+                        reverse=True
+                    )[:10]
+
         return jsonify(results)
     else:
         return "Nay!", 202
--- static/main.js
+++ static/main.js
@@ -1,4 +1,3 @@
-
 (
 	function(){
 		"use strict";
@@ -6,23 +5,37 @@
 		angular.module("WordcountApp", [])
 		.controller("WordcountController", ["$scope", "$log", "$timeout", "$http",
 			function($scope, $log, $timeout, $http){
-				$scope.getResults = function(){
+				$scope.submitButtonText = "Submit";
+				$scope.loading = false;
+				$scope.urlerror = false;
+
+				$scope.getResults = () => {
 					const getWordCount = (jobId) => {
 						let timeout = "";
 						const poller = () => {
 							$http.get("/results/" + jobId).
-								success((data, status, headers, config) => {
+								then((response) => {
+									const {data, status} = response;
+
 									if(status === 202){
 										$log.log(data, status);
 									}
 									else if(status === 200){
-										$log.log(data);
 										$scope.wordcounts = data;
+										$scope.loading = false;
+										$scope.submitButtonText = "Submit";
+										$scope.wordcounts = data;
+
 										$timeout.cancel(timeout);
 
 										return false;
 									}
 									timeout = $timeout(poller, 2000);
+								},
+								(error) => {
+									$scope.urlerror = true;
+									$scope.submitButtonText = "Submit";
+									$scope.loading = false;
 								});
 							};
 						poller();
@@ -30,8 +43,18 @@
 
 					let url = $scope.url;
 					$http.post("/start", { url }).
-						success((results) => getWordCount(results, $log, $http, $timeout)).
-						error((error) => $log.log(error));
+						then((results) => {
+							getWordCount(results.data, $log, $http, $timeout);
+							$scope.urlerror = false;
+							$scope.wordcounts = null;
+							$scope.loading = true;
+							$scope.submitButtonText = "Loading..";
+						}, 
+						(error) => {
+							$scope.urlerror = true;
+							$scope.submitButtonText = "Submit";
+							$scope.loading = false;
+						});
 				};
 			}
 		]);
